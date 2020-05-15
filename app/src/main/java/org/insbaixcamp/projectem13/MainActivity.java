ef666e147f8129e041074c79fbed5e17df992a4a
TiffanyFA <56791305+TiffanyFA@users.noreply.github.com>
2020-05-15 18:32:50 +0200

    primeres actitivities

--- app/src/main/java/org/insbaixcamp/projectem13/MainActivity.java
+++ app/src/main/java/org/insbaixcamp/projectem13/MainActivity.java
@@ -2,7 +2,9 @@ package org.insbaixcamp.projectem13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intentHome = new Intent(this,HomeActivity.class);
+        startActivity(intentHome);
+
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.exit(0);
+    }
+
 }
