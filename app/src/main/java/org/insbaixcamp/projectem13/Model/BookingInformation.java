939abd1b2f67bff6c41f38b82060330f2b4de511
TiffanyFA <56791305+TiffanyFA@users.noreply.github.com>
2020-05-30 16:11:41 +0200

    pujar projecte fet en mode local - login, calendar,confirm

---
+++ app/src/main/java/org/insbaixcamp/projectem13/Model/BookingInformation.java
@@ -0,0 +1,81 @@
+package org.insbaixcamp.projectem13.Model;
+
+import com.google.firebase.Timestamp;
+
+public class BookingInformation {
+    private String customerId;
+    private String customerName;
+    private String customerPhone;
+    private String time;
+    private Long slot;
+    private Timestamp timestamp;
+    private boolean done;
+
+    public BookingInformation() {
+    }
+
+    public BookingInformation(String customerId, String customerName, String customerPhone, String time, Long slot) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerPhone = customerPhone;
+        this.time = time;
+        this.slot = slot;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Long getSlot() {
+        return slot;
+    }
+
+    public void setSlot(Long slot) {
+        this.slot = slot;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+}
